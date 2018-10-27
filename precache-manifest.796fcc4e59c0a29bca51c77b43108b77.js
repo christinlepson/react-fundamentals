ec7fe4ee75234d9ce6d0c5505ceb01f60a6ed64f
Christin Nadya Lepson <christin@protonmail.com>
2018-10-26 21:17:31 -0400

    Updates

---
+++ precache-manifest.796fcc4e59c0a29bca51c77b43108b77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1fdd63a44870b1e29504",
+    "url": "/static/js/main.1fdd63a4.chunk.js"
+  },
+  {
+    "revision": "60f3a0a506d1dfe70d56",
+    "url": "/static/js/1.60f3a0a5.chunk.js"
+  },
+  {
+    "revision": "1fdd63a44870b1e29504",
+    "url": "/static/css/main.8e7ff195.chunk.css"
+  },
+  {
+    "revision": "3e9cb4cd6f0e75981e874f00577f66c2",
+    "url": "/index.html"
+  }
+];
